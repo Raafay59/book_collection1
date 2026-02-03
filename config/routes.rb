e788afb1d6b595f5e0e8211dafca05fee4f2dd9d
Raafay59 <raafay.ah@gmail.com>
2026-02-02 20:52:40 -0600

    BC4

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  root "books#index"
-  resources :books
+  resources :user_books
+  resources :users
+  root 'user_books#index'
+
+  resources :books do
+    member do
+      get :delete
+    end
+  end
 end
